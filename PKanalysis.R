8c666dcaf9b9ee6d547a2a131001e80b7ef13355
Sam Jones <sam.jones@lstmed.ac.uk>
2021-01-06 13:36:48 +0000

    working through mrg

--- PKanalysis.R
+++ PKanalysis.R
@@ -159,48 +159,85 @@ Study1_1cmt <- ggplot()+
   labs(x="Time")+
   ggtitle("Geometric means of in vitro concentration and mrgsolve predicted concentrations",subtitle="Study 1, Compound A, 1 compartment model")
 
-ggsave("Study1_preds_1cmt.tiff")
+#ggsave("Study1_preds_1cmt.tiff")
 #######
 # 2 comp works better...
 #######
 # Want to compare Study 3 with 1 and 2, basically. I guess LWS is the best optimizer?? 
 
 Study3concACombi <- export_conc_combi_doses(get_individual_study(PKPDdata,"Study3"),"CpdA")
-Study3PredACombi <-mrg_model(get_mrgdata_3("Study3","Combination","CpdA"),2,"LWS","pred")
+Study3PredACombi_2 <-mrg_model(get_mrgdata_3("Study3","Combination","CpdA"),2,"LWS","pred")
 
 Study3concBCombi <- export_conc_combi_doses(get_individual_study(PKPDdata,"Study3"),"CpdB")
-Study3PredBCombi <-mrg_model(get_mrgdata_3("Study3","Combination","CpdB"),2,"LWS","pred")
+Study3PredBCombi_2 <-mrg_model(get_mrgdata_3("Study3","Combination","CpdB"),2,"LWS","pred")
+
+
+Study3PredACombi_1 <-mrg_model(get_mrgdata_3("Study3","Combination","CpdA"),1,"LWS","pred")
+Study3PredACombi_3 <-mrg_model(get_mrgdata_3("Study3","Combination","CpdA"),3,"LWS","pred")
+
+Study3PredBCombi_1 <-mrg_model(get_mrgdata_3("Study3","Combination","CpdB"),1,"LWS","pred")
+Study3PredBCombi_3 <-mrg_model(get_mrgdata_3("Study3","Combination","CpdB"),3,"LWS","pred")
 
 Study3_A_geo_conc <- get_geo_mean(Study3concACombi)
 Study3_B_geo_conc <- get_geo_mean(Study3concBCombi)
 
 
-Study3_A_pred <- drop_na(Study3PredACombi)%>%
+# 1 cmt
+Study3_A_pred_1 <- drop_na(Study3PredACombi_1)%>%
   add_dose_data(conc=Study3concACombi)%>%
   select(time,ID,mrgpred,Dose)%>%
   rename(NT=time,DV=mrgpred)%>%
   mutate(method="LWS")%>%
   relocate(ID,NT,DV,Dose,method)
 
-
-Study3_B_pred <- drop_na(Study3PredBCombi)%>%
+Study3_B_pred_1 <- drop_na(Study3PredBCombi_1)%>%
   add_dose_data(conc=Study3concBCombi)%>%
   select(time,ID,mrgpred,Dose)%>%
   rename(NT=time,DV=mrgpred)%>%
   mutate(method="LWS")%>%
   relocate(ID,NT,DV,Dose,method)
 
-Study3_A_geo <- get_geo_mean(Study3_A_pred)
-Study3_B_geo <- get_geo_mean(Study3_B_pred)
+# 2 cmt
 
+Study3_A_pred_2 <- drop_na(Study3PredACombi_2)%>%
+  add_dose_data(conc=Study3concACombi)%>%
+  select(time,ID,mrgpred,Dose)%>%
+  rename(NT=time,DV=mrgpred)%>%
+  mutate(method="LWS")%>%
+  relocate(ID,NT,DV,Dose,method)
+
+Study3_B_pred_2 <- drop_na(Study3PredBCombi_2)%>%
+  add_dose_data(conc=Study3concBCombi)%>%
+  select(time,ID,mrgpred,Dose)%>%
+  rename(NT=time,DV=mrgpred)%>%
+  mutate(method="LWS")%>%
+  relocate(ID,NT,DV,Dose,method)
+
+Study3_A_geo_2 <- get_geo_mean(Study3_A_pred_2)
+Study3_B_geo_2 <- get_geo_mean(Study3_B_pred_2)
+
+Study3_A_geo_1 <- get_geo_mean(Study3_A_pred_1)
+Study3_B_geo_1 <- get_geo_mean(Study3_B_pred_1)
 
 Study3_A <- ggplot()+
   geom_line(data=Study3_A_geo_conc,aes(x=NT,y=GeoMean))+
-  geom_line(data=Study3_A_geo,aes(x=NT,y=GeoMean),color="red",linetype="dotted",size=1)+
+  geom_line(data=Study3_A_geo_2,aes(x=NT,y=GeoMean),color="red",linetype="dotted",size=1)+
+  geom_line(data=Study3_A_geo_1,aes(x=NT,y=GeoMean),color="blue",linetype="dotted",size=1)+
   facet_grid(.~Dose)+
   scale_y_log10()+
   coord_cartesian(ylim=c(0.000001,10))+
   theme_bw()
+
+
+Study3_B <- ggplot()+
+  geom_line(data=Study3_B_geo_conc,aes(x=NT,y=GeoMean))+
+  geom_line(data=Study3_B_geo_2,aes(x=NT,y=GeoMean),color="red",linetype="dotted",size=1)+
+  geom_line(data=Study3_B_geo_1,aes(x=NT,y=GeoMean),color="blue",linetype="dotted",size=1)+
+  facet_grid(.~Dose)+
+  scale_y_log10()+
+  coord_cartesian(ylim=c(0.000001,10))+
+  theme_bw()
+
 
 # Try 3 cmt?
 Study3PredA_3 <-mrg_model(get_mrgdata_3("Study3","Combination","CpdA"),3,"LWS","pred")
@@ -224,134 +261,16 @@ Study3_A_3cmt <- ggplot()+
   theme_bw()
 
 ########
-# Additional runs
+# PArmameter comparisons between mono / combi:
 ########
 
-# Study 1
 
-Study1Param_1cmt <-mrg_model(get_mrgdata_12("Study1","CpdA"),1,"LWS","params")
-Study1Pred_1cmt <-mrg_model(get_mrgdata_12("Study1","CpdA"),1,"LWS","pred")
-
-Study1Param_2cmt <-mrg_model(get_mrgdata_12("Study1","CpdA"),2,"LWS","params")
-Study1Pred_2cmt <-mrg_model(get_mrgdata_12("Study1","CpdA"),2,"LWS","pred")
+Study1Param_CPDA_2 <-mrg_model(get_mrgdata_12("Study1","CpdA"),2,"LWS","params")
+Study2Param_CPDB_2 <-mrg_model(get_mrgdata_12("Study2","CpdB"),2,"LWS","params")
 
 
-Study1Param_OLS <-mrg_model(get_mrgdata_12("Study1","CpdA"),2,"OLS","params")
-Study1Pred_OLS <-mrg_model(get_mrgdata_12("Study1","CpdA"),2,"OLS","pred")
+Study1Param_CPDA_2_min <-mrg_model(get_mrgdata_12("Study1","CpdA"),2,"OLS_min","params")
+Study1Param_CPDA_2_min <-mrg_model(get_mrgdata_12("Study1","CpdA"),2,"OLS_min","pred")
 
-
-Study1Param_OLS1 <-mrg_model(get_mrgdata_12("Study1","CpdA"),1,"OLS","params")
-Study1Pred_OLS1 <-mrg_model(get_mrgdata_12("Study1","CpdA"),1,"OLS","pred")
-
-# Study 2
-Study2Param_1cmt <-mrg_model(get_mrgdata_12("Study2","CpdB"),1,"LWS","params")
-Study2Pred_1cmt <-mrg_model(get_mrgdata_12("Study2","CpdB"),1,"LWS","pred")
-
-Study2Param_2cmt <-mrg_model(get_mrgdata_12("Study2","CpdB"),2,"LWS","params")
-Study2Pred_2cmt <-mrg_model(get_mrgdata_12("Study2","CpdB"),2,"LWS","pred")
-
-# Read in concentration data
-
-Study1conc <- get_conc3(get_individual_study(PKPDdata,"Study1"))
-# Although in all honesty the mrg functions might be better i.e. 
-Study1conc2 <- export_conc_mono(get_individual_study(PKPDdata,"Study1"),"CpdA")
-
- 
-Study2conc2 <- export_conc_mono(get_individual_study(PKPDdata,"Study2"),"CpdB")
-
-# We want to visually compare the actual conc and the predictions, so i.e.:
-# Notes: using conc from study_pred output is awkward as need to remove EVID. 
-
-
-gg_conc <- ggplot()+
-  geom_line(data=Study1conc2,aes(x=NT,y=DV,group=ID,color=as.factor(ID)))+
-  geom_line(data=Study1Pred_2cmt,aes(x=time,y=mrgpred,group=ID,color=as.factor(ID)),linetype="dashed")+
-  facet_grid(ID~.)+
-  scale_y_log10()
-
-
-gg_conc <- ggplot()+
-  geom_line(data=Study1conc2,aes(x=NT,y=DV,group=ID,color=as.factor(ID)))+
-  geom_line(data=Study1Pred_1cmt,aes(x=time,y=mrgpred,group=ID,color=as.factor(ID)),linetype="dashed")+
-  facet_grid(ID~.)+
-  scale_y_log10()
-
-
-gg_conc <- ggplot()+
-  geom_line(data=Study2conc2,aes(x=NT,y=DV,group=ID,color=as.factor(ID)))+
-  geom_line(data=Study2Pred_2cmt,aes(x=time,y=mrgpred,group=ID,color=as.factor(ID)),linetype="dashed")+
-  facet_grid(ID~.)+
-  scale_y_log10()
-
-
-gg_conc <- ggplot()+
-  geom_line(data=Study2conc2,aes(x=NT,y=DV,group=ID,color=as.factor(ID)))+
-  geom_line(data=Study2Pred_1cmt,aes(x=time,y=mrgpred,group=ID,color=as.factor(ID)),linetype="dashed")+
-  facet_grid(ID~.)+
-  scale_y_log10()
-
-# Study 3.. 
-
-Study3concAMono <- export_conc_mono(get_individual_study(PKPDdata,"Study3"),"CpdA")
-
-Study3PredAMono <-mrg_model(get_mrgdata_3("Study3","CpdA","CpdA"),2,"LWS","pred")
-
-
-Study3concBMono <- export_conc_mono(get_individual_study(PKPDdata,"Study3"),"CpdB")
-
-Study3PredBMono <-mrg_model(get_mrgdata_3("Study3","CpdB","CpdB"),2,"LWS","pred")
-
-
-
-Study3concACombi <- export_conc_combi(get_individual_study(PKPDdata,"Study3"),"CpdA")
-
-Study3PredACombi <-mrg_model(get_mrgdata_3("Study3","Combination","CpdA"),2,"LWS","pred")
-
-
-Study3concBCombi <- export_conc_combi(get_individual_study(PKPDdata,"Study3"),"CpdB")
-
-Study3PredBCombi <-mrg_model(get_mrgdata_3("Study3","Combination","CpdB"),2,"LWS","pred")
-
-# Params
-
-
-Study3ParamAMono<-mrg_model(get_mrgdata_3("Study3","CpdA","CpdA"),2,"LWS","params")
-Study3ParamBMono<-mrg_model(get_mrgdata_3("Study3","CpdB","CpdB"),2,"LWS","params")
-
-Study3ParamACombi <-mrg_model(get_mrgdata_3("Study3","Combination","CpdA"),2,"LWS","params")
-Study3ParamBCombi <-mrg_model(get_mrgdata_3("Study3","Combination","CpdB"),2,"LWS","params")
-
-
-gg_conc <- ggplot()+
-  geom_line(data=Study3concAMono,aes(x=NT,y=DV,group=ID,color=as.factor(ID)))+
-  geom_line(data=Study3PredAMono,aes(x=time,y=mrgpred,group=ID,color=as.factor(ID)),linetype="dashed")+
-  facet_grid(ID~.)+
-  scale_y_log10()
-
-
-gg_conc <- ggplot()+
-  geom_line(data=Study3concBMono,aes(x=NT,y=DV,group=ID,color=as.factor(ID)))+
-  geom_line(data=Study3PredBMono,aes(x=time,y=mrgpred,group=ID,color=as.factor(ID)),linetype="dashed")+
-  facet_grid(ID~.)+
-  scale_y_log10()
-
-
-gg_conc <- ggplot()+
-  geom_line(data=Study3concACombi,aes(x=NT,y=DV,group=ID,color=as.factor(ID)))+
-  geom_line(data=Study3PredACombi,aes(x=time,y=mrgpred,group=ID,color=as.factor(ID)),linetype="dashed")+
-  facet_grid(ID~.)+
-  scale_y_log10()
-
-
-gg_conc <- ggplot()+
-  geom_line(data=Study3concBCombi,aes(x=NT,y=DV,group=ID,color=as.factor(ID)))+
-  geom_line(data=Study3PredBCombi,aes(x=time,y=mrgpred,group=ID,color=as.factor(ID)),linetype="dashed")+
-  facet_grid(ID~.)+
-  scale_y_log10()
-
-
-##########
-# Repeat with OLS_min 
-
-Study3ParamA_min <-mrg_model(get_mrgdata_3("Study3","Combination","CpdA"),2,"OLS_min","params")
-Study3PredA_min <-mrg_model(get_mrgdata_3("Study3","Combination","CpdA"),2,"OLS_min","pred")
+Study2Param_CPDB_2_min <-mrg_model(get_mrgdata_12("Study2","CpdB"),2,"OLS_min","params")
+Study2Param_CPDB_2_min <-mrg_model(get_mrgdata_12("Study2","CpdB"),2,"OLS_min","pred")
